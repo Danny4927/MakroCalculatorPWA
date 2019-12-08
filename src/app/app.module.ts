8b88bb306c5d85ca6a9057398aee7394ffdd8b67
Daniel <danielknoell@gmx.de>
2019-12-08 19:02:49 +0100

    add RMR calculation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatButtonModule, MatFormFieldModule, MatInputModule, MatStepperModule, MatToolbarModule} from '@angular/material';
+import {
+  MatButtonModule,
+  MatCardModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatRadioModule,
+  MatStepperModule,
+  MatToolbarModule
+} from '@angular/material';
 import { IntroComponent } from './intro/intro.component';
 import { RmrStepComponent } from './rmr-step/rmr-step.component';
-import {ReactiveFormsModule} from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 
@@ -27,7 +35,10 @@ import { environment } from '../environments/environment';
     MatFormFieldModule,
     MatButtonModule,
     MatInputModule,
-    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
+    ServiceWorkerModule.register('ngsw-worker.js', {enabled: environment.production}),
+    MatRadioModule,
+    MatCardModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/rmr-step/rmr-step.component.ts
+++ src/app/rmr-step/rmr-step.component.ts
@@ -1,5 +1,4 @@
-import { Component, OnInit } from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-rmr-step',
@@ -7,14 +6,50 @@ import {FormBuilder, FormGroup, Validators} from '@angular/forms';
   styleUrls: ['./rmr-step.component.scss']
 })
 export class RmrStepComponent implements OnInit {
-  secondFormGroup: FormGroup;
+  gender: string;
+  age: string;
+  weight: number;
+  calories: number;
 
-  constructor(private _formBuilder: FormBuilder) { }
-
-  ngOnInit() {
-    this.secondFormGroup = this._formBuilder.group({
-      secondCtrl: ['', Validators.required]
-    });
+  constructor() {
   }
 
+  ngOnInit() {
+  }
+
+  calculateRMR() {
+    if (this.age && this.weight && this.gender) {
+      this.calories = this.doCalculation();
+    } else {
+      this.calories = undefined;
+    }
+    console.log('age: ' + this.age + ', weight: ' + this.weight + ', gender: ' + this.gender);
+  }
+
+  private doCalculation() {
+    // male
+    if (this.gender === 'male') {
+      switch (this.age) {
+        case '0':
+          return (17.5 * this.weight) + 651;
+        case '1':
+          return (15.3 * this.weight) + 679;
+        case '2':
+          return (11.6 * this.weight) + 879;
+        case '3':
+          return (13.5 * this.weight) + 487;
+      }
+    } else if (this.gender === 'female') {
+      switch (this.age) {
+        case '0':
+          return (12.2 * this.weight) + 749;
+        case '1':
+          return (14.7 * this.weight) + 496;
+        case '2':
+          return (8.7 * this.weight) + 829;
+        case '3':
+          return (10.5 * this.weight) + 596;
+      }
+    }
+  }
 }
